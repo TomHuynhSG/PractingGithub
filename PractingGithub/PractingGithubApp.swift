f40f23d4e9c29ef730ee7783ed89ad449be2adae
TomHuynhSG <tomhuynhsg@gmail.com>
2026-07-20 08:41:37 +0700

    Initial Commit

---
+++ PractingGithub/PractingGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PractingGithubApp.swift
+//  PractingGithub
+//
+//  Created by Tom Huynh on 20/7/26.
+//
+
+import SwiftUI
+
+@main
+struct PractingGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
